28ba95e14fb40d4bd205fcff5c419ea4112142ca
mhung <hungthiendoan@gmail.com>
2022-06-26 23:54:03 +0700

    update hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,42 +1,31 @@
-import React from 'react';
-// const TodoList = () => {
+import React, { useState } from 'react';
+
+//using hook
+const TodoList = () => {
+  const [name, setName] = useState("mhung")
+  return (
+    <div>
+      <label>Your Name</label>
+      <input type="text" onChange={(event) => setName(event.target.value)} />
+      <br />
+      Hello TodoList with name = {name}
+    </div>
+  );
+};
+// using class
+// class TodoList extends React.Component {
+//   state = {
+//     name: 'mhung',
+//   }
+//   render() {
 //     return (
-//         <div>
-//             Hello Todo List
-//         </div>
-//     );
-// };
+//       <div>
+//         <label>Your name: </label>
+//         <input type="text" onChange={(event) => this.setState({ name: event.target.value })}></input>
+//         Hello {this.state.name}
+//       </div>
+//     )
+//   }
+// }
 
-class TodoList extends React.Component{
-
-  // //Khai báo state
-  // state = {
-  //   name : 'mhung',
-  //   age : '21'
-  // }
-
-  // //gán lại giá trị cho state
-  // render(){
-  //   return (
-  //     <div>
-  //       <label>Name: </label>
-  //       <input type="text" onChange={(event)=> this.setState({name: event.target.value})}/> <br/>
-  //       Hello Todo List name: {this.state.name} and age: {this.state.age}
-  //     </div>
-  //   )
-  // }
-
-  state = {
-    name: 'mhung',
-  }
-  render(){
-    return(
-      <div>
-        <label>Your name: </label>
-        <input type= "text" onChange={(event) => this.setState({name: event.target.value})}></input>
-        Hello {this.state.name}
-      </div>
-    )
-  }
-}
 export default TodoList;
